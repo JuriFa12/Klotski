5a3328944aac3ddd107e5ffa94728c4051e3b1a2
JuriFa12 <76942636+JuriFa12@users.noreply.github.com>
2023-05-15 15:00:18 +0200

    Add files via upload

--- Klotski-main/Klotski-main/KlotSki/src/model/Board.java
+++ Klotski-main/Klotski-main/KlotSki/src/model/Board.java
@@ -11,6 +11,7 @@ public class Board
     int width;
     int configuration;
     int moves;
+    boolean win;
 
     //costruttore iniziale
     public Board ()
@@ -20,6 +21,7 @@ public class Board
         this.width = 4;
         this.configuration = 1;
         this.moves = 0;
+        this.win=false;
 
     }
     //setta le coordinate dei pezzi una volta scelta la configurazione
@@ -40,7 +42,40 @@ public class Board
             return;
         }
         else 
-        return;
+        {
+            if (configuration == 2)
+            {
+                pieces[0] = new Piece(1,1,2,1);
+                pieces[1] = new Piece(1,2,2,2);
+                pieces[2] = new Piece(1,4,1,1);
+                pieces[3] = new Piece(2,4,1,1);
+                pieces[4] = new Piece(3,1,2,1);
+                pieces[5] = new Piece(3,2,1,2);
+                pieces[6] = new Piece(3,4,2,1);
+                pieces[7] = new Piece(4,2,1,1);
+                pieces[8] = new Piece(4,3,2,1);
+                pieces[9] = new Piece(5,2,1,1);
+                return;
+            }
+            else
+            {
+                if (configuration == 3)
+                {
+                    pieces[0] = new Piece(1,1,1,1);
+                    pieces[1] = new Piece(1,2,2,2);
+                    pieces[2] = new Piece(1,4,1,1);
+                    pieces[3] = new Piece(2,1,2,1);
+                    pieces[4] = new Piece(2,4,2,1);
+                    pieces[5] = new Piece(3,2,2,1);
+                    pieces[6] = new Piece(4,1,1,1);
+                    pieces[7] = new Piece(4,4,1,1);
+                    pieces[8] = new Piece(5,1,1,2);
+                    pieces[9] = new Piece(5,3,1,2);
+                    return;
+                }
+            }
+        }
+        
     }
     //setta la configurazione 
     public void set_configuration(int config)
@@ -80,6 +115,25 @@ public class Board
             }
         }
         return -1;
+    }
+    //funzione usata per controllare se si ha vinto o meno
+    public boolean check_win()
+    {
+        int i = 0;
+        int [] dimensions = this.pieces[i].get_dimensions();
+        boolean found = false;
+
+        while(!found)
+        {
+            dimensions = this.pieces[i].get_dimensions();
+            if ((dimensions[2] == 2) && (dimensions[3] == 2))
+                found = true;
+            i++;    
+        }
+
+        if ( (dimensions[0] == 5) && (dimensions[1] == 2))
+            return true;
+        else return false;    
     }
 
     
