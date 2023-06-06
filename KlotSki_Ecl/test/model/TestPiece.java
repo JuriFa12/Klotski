cbcc415a4ccf1ad873061c484968977ac5015492
Tomswnd <92922360+Tomswnd@users.noreply.github.com>
2023-06-06 15:14:52 +0200

    Add files via upload

--- KlotSki_Ecl/test/model/TestPiece.java
+++ KlotSki_Ecl/test/model/TestPiece.java
@@ -6,53 +6,60 @@ import org.junit.jupiter.api.Test;
 class TestPiece {
 
 	@Test
-	public void testPiece() 
-	{
-		new Piece (0,1,2,2);
-		new Piece (1,2,2,2);
+	// Test del costruttore di Piece
+	public void testPiece() {
+		// Crea due istanze di Piece
+		new Piece(0, 1, 2, 2);
+		new Piece(1, 2, 2, 2);
 	}
-	
+
 	@Test
-	public void testPiece2()
-	{
-		Piece a = new Piece (1,1,2,2);
-		int [] n = a.getDimensions();
+	// Test del costruttore di Piece e del metodo getDimensions
+	public void testPiece2() {
+		// Crea un'istanza di Piece
+		Piece a = new Piece(1, 1, 2, 2);
+		int[] n = a.getDimensions();
 		
-		Piece testPiece = new Piece (n[0],n[1],n[2],n[3]);
-		int [] dimensions = testPiece.getDimensions();
+		// Crea un'altra istanza di Piece utilizzando i valori restituiti da getDimensions
+		Piece testPiece = new Piece(n[0], n[1], n[2], n[3]);
+		int[] dimensions = testPiece.getDimensions();
 		
-		assertEquals(dimensions[0],1);
-		assertEquals(dimensions[1],1);
-		assertEquals(dimensions[2],2);
-		assertEquals(dimensions[3],2);
-		
+		// Verifica che le dimensioni siano corrette
+		assertEquals(dimensions[0], 1);
+		assertEquals(dimensions[1], 1);
+		assertEquals(dimensions[2], 2);
+		assertEquals(dimensions[3], 2);
 	}
-	
+
 	@Test
-	public void testMove()
-	{
-		Piece testPiece = new Piece(1,1,2,2);
+	// Test del metodo move
+	public void testMove() {
+		// Crea un'istanza di Piece
+		Piece testPiece = new Piece(1, 1, 2, 2);
 		testPiece.move(1, 2);
 		
-		int [] dimensions = testPiece.getDimensions();
+		// Ottiene le dimensioni dopo lo spostamento
+		int[] dimensions = testPiece.getDimensions();
 		
-		assertEquals(dimensions[0],1);
-		assertEquals(dimensions[1],2);
-		assertEquals(dimensions[2],2);
-		assertEquals(dimensions[3],2);
-		
+		// Verifica che le dimensioni siano corrette dopo lo spostamento
+		assertEquals(dimensions[0], 1);
+		assertEquals(dimensions[1], 2);
+		assertEquals(dimensions[2], 2);
+		assertEquals(dimensions[3], 2);
 	}
-	
+
 	@Test
-	public void testGetDimensions()
-	{
-		Piece testPiece = new Piece (1,1,2,1);
-		int [] dimensions = testPiece.getDimensions();
+	// Test del metodo getDimensions
+	public void testGetDimensions() {
+		// Crea un'istanza di Piece
+		Piece testPiece = new Piece(1, 1, 2, 1);
+		int[] dimensions = testPiece.getDimensions();
 		
-		assertEquals(dimensions[0],1);
-		assertEquals(dimensions[1],1);
-		assertEquals(dimensions[2],2);
-		assertEquals(dimensions[3],1);
+		// Verifica che le dimensioni siano corrette
+		assertEquals(dimensions[0], 1);
+		assertEquals(dimensions[1], 1);
+		assertEquals(dimensions[2], 2);
+		assertEquals(dimensions[3], 1);
 	}
 
 }
